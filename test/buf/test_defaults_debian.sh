594c679af832665c6e307a28681dcf92169ac71f
marcozac <marco@marcozaccaro.com>
2023-08-03 17:27:30 +0200

    test(buf): add `protoc-gen-buf-breaking` and `protoc-gen-buf-lint` checks

--- test/buf/test_defaults_debian.sh
+++ test/buf/test_defaults_debian.sh
@@ -5,5 +5,7 @@ set -e
 source dev-container-features-test-lib
 
 check "buf --version" buf --version
+check "check 'protoc-gen-buf-breaking' is in the PATH" type protoc-gen-buf-breaking
+check "check 'protoc-gen-buf-lint' is in the PATH" type protoc-gen-buf-lint
 
 reportResults
