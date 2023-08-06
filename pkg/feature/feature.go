7bf1ac6536bf1c96905d1ae752d93c07797e543d
marcozac <marco@marcozaccaro.com>
2023-08-06 11:15:51 +0200

    refactor(feature): support comments in `devcontainer-feature.json`

--- pkg/feature/feature.go
+++ pkg/feature/feature.go
@@ -1,10 +1,11 @@
 package feature
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
+
+	"github.com/marcozac/go-jsonc"
 )
 
 type Feature struct {
@@ -24,7 +25,7 @@ func FromDirPath(dir string) (*Feature, error) {
 		return nil, fmt.Errorf("read feature file: %w", err)
 	}
 	feat := new(Feature)
-	if err := json.Unmarshal(data, feat); err != nil {
+	if err := jsonc.Unmarshal(data, feat); err != nil {
 		return nil, fmt.Errorf("unmarshal feature file: %w", err)
 	}
 	return feat, nil
